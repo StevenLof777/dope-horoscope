d05f544651254e176005c979009e708f26e52953
Jake Finn <lof.steven@gmail.com>
2022-01-04 18:39:08 -0600

    Add drop down

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
+var inputEl = document.querySelector('#userInput')
+var input = inputEl.value
 
-
-fetch("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=libra&day=today", {
+fetch(`"https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=libra&${day=today}"`, {
 	"method": "POST",
 	"headers": {
 		"x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
@@ -9,15 +10,17 @@ fetch("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=libra&day=today", {
 })
 .then(response => {
 
-    var something = response.json()
-    .then(function (something){
-        console.log(something.color)
+    var horoscopeData = response.json()
+    .then(function (horoscopeData){
+        console.log(horoscopeData)
     })
-    console.log(something)
+    // console.log(something)
 })
 .catch(err => {
 	console.error(err);
 });
+
+
 
 // mM3hOIKpYsVAmBopD3qVFA==mPctNJmaPTOScjRr
 
@@ -30,9 +33,22 @@ $.ajax({
     headers: { 'X-Api-Key': 'mM3hOIKpYsVAmBopD3qVFA==mPctNJmaPTOScjRr'},
     contentType: 'application/json',
     success: function(result) {
-        console.log(result);
+        // console.log(result);
+		var celebBDay = result[0].birthdy
+		// console.log(celebBDay)
+		// console.log(celebBDay.slice(5))
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
 });
+
+// function testingFunc() {
+// 	var zodiac = '02-17' 
+// 	if (celebBDay.slice(5) != zodiac) {
+// 		console.log('words')
+// 	} else {
+// 		console.log("doesn't work")
+// 	}
+// }
+// testingFunc()
