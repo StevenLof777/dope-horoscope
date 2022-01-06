4edad538c111ff57d882115480d73ac5e78d1807
Jake Finn <lof.steven@gmail.com>
2022-01-06 10:31:36 -0600

    pulling from main

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// User Details
 var inputEl = document.querySelector('#month1')
 var input = inputEl.value
 
@@ -15,7 +16,8 @@ var moodLi = document.querySelector('#mood')
 var zodiacArr =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
 var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
 
-
+// Celebrity Names
+var name = 'Michael Jackson'
 var Aries =["Emma Watson", "Paul Rudd"]
 var Taurus =["Kelly Clarkson", "George Klooney"]
 var Gemini =["Venus Williams", "Johnny Depp"]
@@ -32,49 +34,65 @@ var Pisces =["Justin Bieber", "Olivia Rodrigo"]
 function zodiac(day, month){
     // returns the zodiac sign according to day and month (https://coursesweb.net/javascript/zodiac-signs_cs)
     return (day > last_day[month]) ? zodiacArr[month*1 + 1] : zodiacArr[month];
+    
 }
 
-var z_sign = zodiac(01, 09);
-console.log()
+var z_sign = 'virgo';
 
-fetch(`https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${z_sign}&day=today`, {
-"method": "POST",
-"headers": {
-    "x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
-    "x-rapidapi-key": "e62a7796a3msh0623a8dce54fae1p10c5c9jsn4bfcafdb2b2b"
-    }
-})
-.then(response => {
-    var horoscopeData = response.json()
-    .then(function (horoscopeData){
-        userSign.innerHTML='Sign: ' + z_sign
-        userColorLI.innerHTML="Color:  " + horoscopeData.color
-        userCompLI.innerHTML="Compatibility:  " + horoscopeData.compatibility
-        userDescLI.innerHTML="Description:  " + horoscopeData.description
-        luckyNumLI.innerHTML="Lucky Number:  " + horoscopeData.lucky_number
-        luckyTimeLI.innerHTML="Lucky Time:  " + horoscopeData.lucky_time
-        moodLi.innerHTML="Mood:  " + horoscopeData.mood
+function callUser () {
+    fetch(`https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${z_sign}&day=today`, {
+    "method": "POST",
+    "headers": {
+        "x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com",
+        "x-rapidapi-key": "e62a7796a3msh0623a8dce54fae1p10c5c9jsn4bfcafdb2b2b"
+        }
     })
-})
-.catch(err => {
-	console.error(err);
-});
+    .then(response => {
+        var horoscopeData = response.json()
+        .then(function (horoscopeData){
+            userSign.innerHTML='Sign: ' + z_sign
+            userColorLI.innerHTML="Color:  " + horoscopeData.color
+            userCompLI.innerHTML="Compatibility:  " + horoscopeData.compatibility
+            userDescLI.innerHTML="Description:  " + horoscopeData.description
+            luckyNumLI.innerHTML="Lucky Number:  " + horoscopeData.lucky_number
+            luckyTimeLI.innerHTML="Lucky Time:  " + horoscopeData.lucky_time
+            moodLi.innerHTML="Mood:  " + horoscopeData.mood
+        })
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+callUser()
 
-var name = 'Michael Jackson'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/celebrity?name=' + name,
-    headers: { 'X-Api-Key': 'mM3hOIKpYsVAmBopD3qVFA==mPctNJmaPTOScjRr'},
-    contentType: 'application/json',
-    success: function(result) {
-		var celebBDay = result[0].birthdy
-        var celebBDayArr = celebBDay.slice(5).split('-')
-        var celebMonth = parseInt(celebBDayArr[0])
-        var celebDay = parseInt(celebBDayArr[1])
-        console.log(celebMonth)
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+// Call Celeb
+function callCeleb () {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/celebrity?name=' + name,
+        headers: { 'X-Api-Key': 'mM3hOIKpYsVAmBopD3qVFA==mPctNJmaPTOScjRr'},
+        contentType: 'application/json',
+        success: function(result) {
+            var celebBDay = result[0].birthdy
+            var celebBDayArr = celebBDay.slice(5).split('-')
+            var celebMonth = parseInt(celebBDayArr[0])
+            var celebDay = parseInt(celebBDayArr[1])
+            console.log(celebMonth)
+            console.log(celebDay)
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+        
+    })
+    // .done
+    // console.log(celebMonth)
+    // console.log(celebDay)
+}
+callCeleb()
+
+// Date Picker
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
 
